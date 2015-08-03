2f0f6c4ee0c185951a6548df455e58f37b8b210a
Spyros Desyllas <sdesyllas@hotmail.com>
2015-08-03 12:40:07 +0300

    game-v1.3

---
+++ RiotApi.Net.RestClient/ApiCalls/Game.cs
@@ -0,0 +1,29 @@
+﻿using RiotApi.Net.RestClient.Interfaces;
+using RiotApi.Net.RestClient.Configuration;
+using RiotApi.Net.RestClient.Dto.Game;
+
+namespace RiotApi.Net.RestClient.ApiCalls
+{
+    public class Game : GenericRiotCaller, IGame
+    {
+        public Game(string apiKey)
+        {
+            this.ApiKey = apiKey;
+        }
+
+        public string ApiKey { get; set; }
+
+        public RecentGamesDto GetRecentGamesBySummonerId(RiotApiConfig.Regions region, long summonerId)
+        {
+            //https://eune.api.pvp.net/api/lol/eune/v1.3/game/by-summoner/41488614/recent?api_key=
+            //find the appropriate end point depending the region
+            var endPoint = RiotApiConfig.GetRegionalEndPointByRegion(region);
+            //compose url
+            var baseUrl = $"https://{endPoint.Host}/";
+            var apiCallPath = $"api/lol/eune/v1.3/game/by-summoner/{summonerId}/recent?api_key={this.ApiKey}";
+            //make the call
+            var dto = MakeCallToRiotApi<RecentGamesDto>(baseUrl, apiCallPath);
+            return dto;
+        }
+    }
+}
---
+++ RiotApi.Net.Tests/ApiCallTests/GameTests.cs
@@ -0,0 +1,62 @@
+﻿using Autofac;
+using NUnit.Framework;
+using RiotApi.Net.RestClient.ApiCalls;
+using RiotApi.Net.RestClient.Helpers;
+using RiotApi.Net.RestClient.Interfaces;
+using System;
+using System.Configuration;
+
+namespace RiotApi.Net.Tests.ApiCallTests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        /// <summary>
+        /// IOC (Inversion of Control) container
+        /// https://en.wikipedia.org/wiki/Inversion_of_control
+        /// </summary>
+        private static IContainer Container { get; set; }
+        private static ILifetimeScope Scope { get; set; }
+        public object RiotApiConfig { get; private set; }
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            // Create your builder.
+            var builder = new ContainerBuilder();
+            // Register individual components
+            builder.RegisterInstance(new Game(ConfigurationManager.AppSettings["ApiKey"])).As<IGame>();
+            Container = builder.Build();
+            Scope = Container.BeginLifetimeScope();
+            Scope.Resolve<IGame>();
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Scope.Dispose();
+        }
+
+        [Test]
+        public void GetRecentGamesBySummonerId()
+        {
+            var api = Scope.Resolve<IGame>();
+            try
+            {
+                var dto = api.GetRecentGamesBySummonerId(RestClient.Configuration.RiotApiConfig.Regions.EUNE, 41488614);
+                Assert.NotNull(dto);
+                Console.WriteLine($"Summoner: {dto.SummonerId}");
+                foreach(var game in dto.Games)
+                {
+                    Assert.IsNotNull(game);
+                }
+                Console.WriteLine(dto.ToString());
+            }
+            catch (RiotExceptionRaiser.RiotApiException riotException)
+            {
+                Console.WriteLine($"Summoner is not currently playing any game! msg : {riotException.Message}. Error:{riotException.RiotErrorCode}");
+            }
+        }
+
+    }
+}
