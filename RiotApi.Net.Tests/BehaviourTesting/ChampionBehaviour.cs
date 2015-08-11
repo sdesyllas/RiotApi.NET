f9313cbda0d03049b5dae7235c677b7d8ba7bc58
Spyros Desyllas <sdesyllas@hotmail.com>
2015-08-11 15:57:13 +0300

    CurrentGameBehaviour

--- RiotApi.Net.Tests/BehaviourTesting/ChampionBehaviour.cs
+++ RiotApi.Net.Tests/BehaviourTesting/ChampionBehaviour.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Moq;
+﻿using Moq;
 using NUnit.Framework;
 using RiotApi.Net.RestClient;
 using RiotApi.Net.RestClient.Configuration;
-using RiotApi.Net.RestClient.Dto.LolStaticData.Champion;
-using RiotApi.Net.RestClient.Dto.Summoner;
 using ChampionListDto = RiotApi.Net.RestClient.Dto.Champion.ChampionListDto;
 
 namespace RiotApi.Net.Tests.BehaviourTesting
 {
+    [TestFixture]
     public class ChampionBehaviour
     {
         private Mock<IRiotClient> _mockClient;
@@ -26,10 +20,15 @@ namespace RiotApi.Net.Tests.BehaviourTesting
         [Test]
         public void RetrieveAllChampionsBehaviour()
         {
+            //Arrange
             _mockClient.Setup(x => x.Champion.RetrieveAllChampions(It.IsAny<RiotApiConfig.Regions>(), It.IsAny<bool>()))
                 .Returns(new ChampionListDto());
+
+            //Act
             IRiotClient client = _mockClient.Object;
             ChampionListDto dto = client.Champion.RetrieveAllChampions(RiotApiConfig.Regions.EUNE, true);
+
+            //Assert
             Assert.IsInstanceOf<ChampionListDto>(dto);
             Assert.NotNull(dto);
         }
@@ -37,10 +36,15 @@ namespace RiotApi.Net.Tests.BehaviourTesting
         [Test]
         public void RetrieveChampionByIdBehaviour()
         {
+            //Arrange
             _mockClient.Setup(x => x.Champion.RetrieveChampionById(It.IsAny<RiotApiConfig.Regions>(), It.IsAny<int>()))
                 .Returns(new ChampionListDto.ChampionDto());
+
+            //Act
             IRiotClient client = _mockClient.Object;
             ChampionListDto.ChampionDto dto = client.Champion.RetrieveChampionById(RiotApiConfig.Regions.EUNE, 8);
+
+            //Assert
             Assert.IsInstanceOf<ChampionListDto.ChampionDto>(dto);
             Assert.NotNull(dto);
         }
---
+++ RiotApi.Net.Tests/BehaviourTesting/CurrentGameBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using RiotApi.Net.RestClient;
+using RiotApi.Net.RestClient.Configuration;
+using RiotApi.Net.RestClient.Dto.CurrentGame;
+
+namespace RiotApi.Net.Tests.BehaviourTesting
+{
+    [TestFixture]
+    public class CurrentGameBehaviour
+    {
+        private Mock<IRiotClient> _mockClient;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            _mockClient = new Mock<IRiotClient>();
+        }
+
+        [Test]
+        public void GetCurrentGameInformationForSummonerIdBehaviour()
+        {
+            //Arrange
+            _mockClient.Setup(x => x.CurrentGame.GetCurrentGameInformationForSummonerId(It.IsAny<RiotApiConfig.Platforms>(), It.IsAny<long>()))
+                .Returns(new CurrentGameInfo());
+
+            //Act
+            IRiotClient client = _mockClient.Object;
+            CurrentGameInfo dto = client.CurrentGame.GetCurrentGameInformationForSummonerId(
+                RiotApiConfig.Platforms.EUN1, 1);
+
+            //Assert
+            Assert.IsInstanceOf<CurrentGameInfo>(dto);
+            Assert.NotNull(dto);
+        }
+    }
+}
