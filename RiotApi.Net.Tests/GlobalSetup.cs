2d75ece29f7ef094516275eeb488e7d62d527346
Spyros Desyllas <sdesyllas@hotmail.com>
2015-09-22 10:06:46 +0300

    Continuous testing environmental variables #15

--- RiotApi.Net.Tests/GlobalSetup.cs
+++ RiotApi.Net.Tests/GlobalSetup.cs
@@ -15,8 +15,10 @@ namespace RiotApi.Net.Tests
         [SetUp]
         public void GlueNinjectModules()
         {
+            //first check the environmental variable if presented, that means we are in an continuous integration environment
+            var apiKey = Environment.GetEnvironmentVariable("ApiKey") ?? ConfigurationManager.AppSettings["ApiKey"];
             //load a Riot Http module with an Api key into your kernel of your app
-            IKernel kernel = new StandardKernel(new RiotHttpClientModule(ConfigurationManager.AppSettings["ApiKey"]));
+            IKernel kernel = new StandardKernel(new RiotHttpClientModule(apiKey));
             //get back the riot client from your application kernel using ninject
             RiotHttpClient = kernel.Get<RiotClient>();
         }
